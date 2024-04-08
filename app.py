0cf4b2ac1293731fa0aca17d9f2742435cc9f1e1
1dayluo <mar1sadaze@qq.com>
2024-04-08 18:13:25 +0800

    update: read local file

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, url_for, render_template, \
+    request, flash, session, Response
+import os
+import pathlib
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4a@@Q8z\n\xec]/'
+base_config_path = str(pathlib.Path.home())+'/.config/subnya'
+log_file = base_config_path + '/log/monitor_run.log'
+output_dir = ''
+sql_path = ''
+
+@app.route('/')
+def home():
+    if len(output_dir) == 0 or len(sql_path) == 0:
+        flash('Path not exists!please set base path ')
+        return redirect(url_for('setenv'))    
+    else:
+        if os.path.exists(output_dir) and os.path.exists(sql_path):
+            # read log file
+            all_files = os.listdir(output_dir)
+            return render_template('home.html', log_files=all_files)
+        else:
+            flash('Please set correct path!')
+            return redirect(url_for('setenv'))
+    return render_template('home.html')
+
+@app.route('/setting', methods=['GET','POST'])
+def setenv():
+    global output_dir, sql_path, base_config_path
+    if request.method == 'POST':
+        output_dir = request.form['output_dir']
+        sql_path = request.form['sql_path']
+        return redirect(url_for('home'))
+    
+    return render_template('setting.html', output_dir=base_config_path+'/output', \
+                           sql_path=base_config_path+'/db/monitor.db')
+
+
+
+@app.route('/outfile/<filename>')
+def outfile(filename=''):
+    global output_dir
+    with open(output_dir + '/' + filename, 'r') as f:
+         content = f.read()
+    return render_template('show_text.html', text_content=content)
+
+@app.route('/logs')
+def log():
+    with open(log_file, 'r') as f:
+        content = f.read()
+
+    return render_template('show_text.html', text_content=content)
